f5b19c612a199e7a2b6e67b9c759d3ea75ed1121
Ciumac Sergiu <ciumac.sergiu@gmail.com>
2015-05-01 22:51:52 +0300

    SF-47 Fixing dependencies after changing repository for Bass.

---
+++ src/SoundFingerprinting.Audio.Bass.Tests/AbstractTest.cs
@@ -0,0 +1,76 @@
+﻿namespace SoundFingerprinting.Tests
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.DAO.Data;
+
+    [DeploymentItem(@"x86", @"x86")]
+    [DeploymentItem(@"x64", @"x64")]
+    [TestClass]
+    public abstract class AbstractTest
+    {
+        protected const double Epsilon = 0.0001;
+
+        protected const int BitsPerSample = 32;
+
+        protected const int SampleRate = 5512;
+
+        protected const int SamplesPerFingerprint = 128 * 64;
+
+        protected const int WaveHeader = 58;
+
+        protected const string PathToMp3 = @"Kryptonite.mp3";
+
+        protected const string PathToSamples = @"floatsamples.bin";
+
+        protected const int SamplesToRead = 128 * 64;
+
+        protected const int MinYear = 1501;
+
+        protected readonly bool[] GenericFingerprint = new[]
+            {
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true, true, false, true, false, true, false, true, false,
+                true, false, true, false, false, true, false, true, false, true, false, true, false, true, false, true,
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true, true, false, true, false, true, false, true, false,
+                true, false, true, false, false, true, false, true, false, true, false, true, false, true, false, true,
+                true, false, true, false, true, false, true, false, true, false, true, false, false, true, false, true,
+                false, true, false, true, false, true, false, true
+            };
+
+        protected readonly byte[] GenericSignature = new[]
+                                                         {
+                                                             (byte)1, (byte)2, (byte)3, (byte)4, (byte)5, (byte)6, (byte)7, (byte)8, (byte)9, (byte)10, (byte)11, (byte)12,
+                                                             (byte)13, (byte)14, (byte)15, (byte)16, (byte)17, (byte)18, (byte)19, (byte)20, (byte)21, (byte)22, (byte)23,
+                                                             (byte)24, (byte)25
+                                                         };
+
+        protected readonly long[] GenericHashBuckets = new[] { 1L, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+
+        protected void AssertTracksAreEqual(TrackData expectedTrack, TrackData actualTrack)
+        {
+            Assert.AreEqual(expectedTrack.TrackReference, actualTrack.TrackReference);
+            Assert.AreEqual(expectedTrack.Album, actualTrack.Album);
+            Assert.AreEqual(expectedTrack.Artist, actualTrack.Artist);
+            Assert.AreEqual(expectedTrack.Title, actualTrack.Title);
+            Assert.AreEqual(expectedTrack.TrackLengthSec, actualTrack.TrackLengthSec);
+            Assert.AreEqual(expectedTrack.ISRC, actualTrack.ISRC);
+            Assert.AreEqual(expectedTrack.GroupId, actualTrack.GroupId);
+        }
+
+        protected void AssertArraysAreEqual<T1, T2>(T1[] expected, T2[] actual)
+            where T1 : IComparable
+            where T2 : IComparable
+        {
+            Assert.AreEqual(expected.Length, actual.Length);
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.IsTrue(expected[i].CompareTo(actual[i]) == 0);
+            }
+        }
+    }
+}
---
+++ src/SoundFingerprinting.Audio.Bass.Tests/Integration/AbstractIntegrationTest.cs
@@ -0,0 +1,52 @@
+﻿namespace SoundFingerprinting.Tests.Integration
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SoundFingerprinting.DAO;
+    using SoundFingerprinting.Data;
+
+    [DeploymentItem(@"TestEnvironment\floatsamples.bin")]
+    [DeploymentItem(@"TestEnvironment\Kryptonite.mp3")]
+    [TestClass]
+    public abstract class AbstractIntegrationTest : AbstractTest
+    {
+        protected void AssertHashDatasAreTheSame(IList<HashedFingerprint> firstHashDatas, IList<HashedFingerprint> secondHashDatas)
+        {
+            Assert.AreEqual(firstHashDatas.Count, secondHashDatas.Count);
+
+            // hashes are not ordered as parallel computation is involved
+            firstHashDatas = SortHashesByFirstValueOfHashBin(firstHashDatas);
+            secondHashDatas = SortHashesByFirstValueOfHashBin(secondHashDatas);
+
+            for (int i = 0; i < firstHashDatas.Count; i++)
+            {
+                for (int j = 0; j < firstHashDatas[i].SubFingerprint.Length; j++)
+                {
+                    Assert.AreEqual(firstHashDatas[i].SubFingerprint[j], secondHashDatas[i].SubFingerprint[j]);
+                }
+
+                for (int j = 0; j < firstHashDatas[i].HashBins.Length; j++)
+                {
+                    Assert.AreEqual(firstHashDatas[i].HashBins[j], secondHashDatas[i].HashBins[j]);
+                }
+
+                Assert.AreEqual(firstHashDatas[i].SequenceNumber, secondHashDatas[i].SequenceNumber);
+                Assert.AreEqual(firstHashDatas[i].Timestamp, secondHashDatas[i].Timestamp, Epsilon);
+            }
+        }
+
+        protected void AssertModelReferenceIsInitialized(IModelReference modelReference)
+        {
+            Assert.IsNotNull(modelReference);
+            Assert.IsTrue(modelReference.GetHashCode() != 0);
+        }
+
+        private List<HashedFingerprint> SortHashesByFirstValueOfHashBin(IEnumerable<HashedFingerprint> hashDatasFromFile)
+        {
+            return hashDatasFromFile.OrderBy(hashData => hashData.HashBins[0]).ToList();
+        }
+    }
+}
