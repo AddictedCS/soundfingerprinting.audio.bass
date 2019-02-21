881ee3479361700ca6a4ab53a5d23433787b5e64
Sergiu Ciumac <sciumac@nemo.ai>
2019-02-21 17:42:23 +0200

    Adding build.sh for OSX or Unix

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    TARGET="Release"
+else
+    TARGET=$1    
+fi
+
+nuget install NUnit.Runners -Version 3.5.0 -OutputDirectory build/testrunner
+
+msbuild src/SoundFingerprinting.Audio.Bass.sln /p:Configuration=Release
+
+mono build/testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe src/SoundFingerprinting.Audio.Bass.Tests/bin/Release/net461/SoundFingerprinting.Audio.Bass.Tests.dll
+
+msbuild /t:Pack src/SoundFingerprinting.Audio.Bass/SoundFingerprinting.Audio.Bass.csproj /p:PackageOutputPath=../../build
