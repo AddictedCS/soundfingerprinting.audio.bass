911762b5fa396f8825f9b596a46b65eec8d69c68
Viorel Ghelbert <5279394+gviorel@users.noreply.github.com>
2019-04-18 18:28:47 +0300

    Allow Debug builds.

--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@ fi
 nuget install NUnit.Runners -Version 3.5.0 -OutputDirectory build/testrunner
 
 msbuild src/SoundFingerprinting.Audio.Bass.sln /t:Restore
-msbuild src/SoundFingerprinting.Audio.Bass.sln /p:Configuration=Release
+msbuild src/SoundFingerprinting.Audio.Bass.sln /p:Configuration=$TARGET
 
-dotnet test ./src/SoundFingerprinting.Audio.Bass.Tests/SoundFingerprinting.Audio.Bass.Tests.csproj -c Release -f netcoreapp2.0
+dotnet test ./src/SoundFingerprinting.Audio.Bass.Tests/SoundFingerprinting.Audio.Bass.Tests.csproj -c $TARGET -netcoreapp2.0 -v n
 
-mono build/testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe src/SoundFingerprinting.Audio.Bass.Tests/bin/Release/net461/SoundFingerprinting.Audio.Bass.Tests.dll
+mono build/testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe src/SoundFingerprinting.Audio.Bass.Tests/bin/$TARGET/net461/SoundFingerprinting.Audio.Bass.Tests.dll
 
 msbuild /t:Pack src/SoundFingerprinting.Audio.Bass/SoundFingerprinting.Audio.Bass.csproj /p:PackageOutputPath=../../build
