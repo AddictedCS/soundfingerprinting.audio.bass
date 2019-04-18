917a270f1593b5b98ffe412c52669048a7a63c03
Viorel Ghelbert <5279394+gviorel@users.noreply.github.com>
2019-04-18 21:29:15 +0300

    Try to run tests with .NET Core in Travis CI.

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ nuget install NUnit.Runners -Version 3.5.0 -OutputDirectory build/testrunner
 msbuild src/SoundFingerprinting.Audio.Bass.sln /t:Restore
 msbuild src/SoundFingerprinting.Audio.Bass.sln /p:Configuration=$TARGET
 
-dotnet test ./src/SoundFingerprinting.Audio.Bass.Tests/SoundFingerprinting.Audio.Bass.Tests.csproj -c $TARGET -netcoreapp2.0 -v n
+dotnet test ./src/SoundFingerprinting.Audio.Bass.Tests/SoundFingerprinting.Audio.Bass.Tests.csproj -c $TARGET -f netcoreapp2.0 -v n
 
 mono build/testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe src/SoundFingerprinting.Audio.Bass.Tests/bin/$TARGET/net461/SoundFingerprinting.Audio.Bass.Tests.dll
 
