4b26c3fd57d25a01ae941e3c4986f39adea09201
Sergiu Ciumac <sciumac@nemo.ai>
2019-05-01 16:32:55 +0300

    Adding methods to be able to write short[] into wave file.

--- src/SoundFingerprinting.Audio.Bass/BassWaveFileUtility.cs
+++ src/SoundFingerprinting.Audio.Bass/BassWaveFileUtility.cs
@@ -1,13 +1,12 @@
 namespace SoundFingerprinting.Audio.Bass
 {
+    using System.Collections.Generic;
     using System.IO;
     using ManagedBass;
     using WaveFormat = ManagedBass.WaveFormat;
 
     public class BassWaveFileUtility : IWaveFileUtility
     {
-        private const int FloatLength = 4;
-
         public void WriteSamplesToFile(float[] samples, int sampleRate, string destination)
         {
             var waveFormat = WaveFormat.CreateIeeeFloat(sampleRate, BassConstants.NumberOfChannels);
@@ -15,8 +14,35 @@ namespace SoundFingerprinting.Audio.Bass
             using (var stream = new FileStream(destination, FileMode.Create))
             using (var waveWriter = new WaveFileWriter(stream, waveFormat))
             {
-                waveWriter.Write(samples, samples.Length * FloatLength);
+                waveWriter.Write(samples, samples.Length * sizeof(float));
             }
+        }
+
+        public void WriteSamplesToFile(short[] samples, int sampleRate, string destination)
+        {
+            var waveFormat = new WaveFormat(sampleRate, BassConstants.NumberOfChannels);
+            using (var stream = new FileStream(destination, FileMode.Create))
+            using (var waveWriter = new WaveFileWriter(stream, waveFormat))
+            {
+                waveWriter.Write(samples, samples.Length * sizeof(short));
+            }
+        }
+
+        public static short[] Convert(IReadOnlyList<float> samples)
+        {
+            short[] result = new short[samples.Count];
+            for (int i = 0; i < samples.Count; ++i)
+            {
+                int value = (int)(samples[i] * short.MaxValue);
+                if (value > short.MaxValue)
+                    value = short.MaxValue;
+                else if (value < short.MinValue)
+                    value = short.MinValue;
+
+                result[i] = (short)value;
+            }
+
+            return result;
         }
     }
 }
